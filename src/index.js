de373999001b4418bf667643804cd962fd0c74de
Anca_Sab <asabadeanu@gmail.com>
2025-03-11 10:43:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/slim-mom-frontend">
+      <BrowserRouter basename="/SlimMom-frontend">
         <App />
       </BrowserRouter>
     </PersistGate>
