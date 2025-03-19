88631199279e939c66c88524e24ea53078e5a692
Anca_Sab <asabadeanu@gmail.com>
2025-03-19 19:30:00 +0200

    Update registerSchema.js

--- src/helpers/validation/registerSchema.js
+++ src/helpers/validation/registerSchema.js
@@ -4,11 +4,11 @@ export const registerSchema = yup.object({
   name: yup
     .string()
     .min(3, 'Min number of characters is 6')
-    .max(15, 'Max number of characters is 15')
+    .max(15, 'Max number of characters is 35')
     .required('Name is required'),
   email: yup.string().email('Invalid email').required('Email is required'),
   password: yup
     .string()
-    .min(6, 'Min number of characters is 6')
+    .min(8, 'Min number of characters is 8')
     .required('Password is required'),
 });
